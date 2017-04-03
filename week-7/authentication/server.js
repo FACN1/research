db9d0453d9235456a393743d2250a265a7da5103
Lawson <lgbeaty@live.ca>
2017-04-03 15:58:18 +0300

    Update server.js

--- week-7/authentication/server.js
+++ week-7/authentication/server.js
@@ -15,6 +15,10 @@ server.connection({
 const users = {
   john: {
     username: 'john',
+    // '$2a$' indicating that it is a bcrypt hashSync
+    // '10$' rounds of hashing
+    // 22 char salting
+    // 31 char of hashing
     password: '$2a$10$iqJSHD.BGr0E2IxQwYgJmeP3NvhPrXAeLSaGCj6IR/XU5QtjVu5Tm',
     // 'secret'
     name: 'John Doe',
